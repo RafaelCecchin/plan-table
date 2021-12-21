8cee4a61d0ad02204b22638d0d497dc62e3582c7
RafaelCecchin <contatorafaelcecchin@gmail.com>
2021-12-21 11:59:23 -0300

    feat: adicionar slider de planos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
+const bpTablet = 1050;
+const bpSmartphone = 700;
+const bpSmall = 550;
+
 const table = $(".table");
-const colPlanos = $(".plans .col");
+const plans = $(".plans");
+const colPlanos = plans.find(".col");
 const colRecursos = $(".resources .col");
 const recursos = colRecursos.children();
 
-function updateDataHeight() {
+const leftArrow = $(".arrows .left-arrow");
+const rightArrow = $(".arrows .right-arrow");
+
+function getResourceWidth() {
+    return colRecursos.parent().outerWidth();
+}
+
+function updateDataSize() {
 
     recursos.each(function () {
 
@@ -20,9 +32,20 @@ function updateDataHeight() {
         let linhasRecursos = recurso.children();
 
         colPlanos.each(function () {
+        
+            let col = $(this);
+            let windowWidth = $(window).width();
+            let plansWidth = plans.width();
+
+            if (windowWidth < bpSmall) {
+                col.width((plansWidth)+"px");
+            } else if (windowWidth < bpTablet) {
+                col.width((plansWidth/2)+"px");
+            } else if (windowWidth >= bpTablet) {
+                col.width("unset");
+            } 
 
             let recursosDoPlano = $(this).find(".data");
-            console.log(recursosDoPlano);
             recursosDoPlano.each(function () {
     
                 let recursoDoPlano = $(this);
@@ -40,6 +63,8 @@ function updateDataHeight() {
             });
         });
     });
+
+    scroll();
 }
 
 function closeAllData() {
@@ -93,41 +118,64 @@ function collapseData() {
     
 }
 
+function scroll(event) {
+    let eventDirection = event ? event.data.direction : null;
+    let containerPosition = table.offset().left + getResourceWidth();
+    let closer;
+    let newScroll = 0;
+
+    /* Esse foreach serve para definir a *
+    *  coluna de plano que está na visão *
+    *  do usuário no momento             */
+    colPlanos.each(function () {
+        let plan = $(this);
+        let planPosition = containerPosition - plan.offset().left;
+        let closerPosition = containerPosition - (closer != null ? closer.offset().left : 0);
+
+        if (Math.abs(planPosition) < Math.abs(closerPosition) || closer == null) {
+            closerProximity = planPosition;
+            closer = plan;
+        }
+    });
+
+    
+    /* Esse foreach serve para pegar a soma do       *
+    *  tamanho das colunas anteriores ou posteriores */ 
+
+    if (eventDirection == "right") {
+
+        if (closer.next().length) {
+            closer = closer.next();
+        }
+
+    } else if (eventDirection == "left") {
+
+        if (closer.prev().length) {
+            closer = closer.prev();
+        }
+        
+    } 
+    
+    closer.prevAll().each(function() {
+        newScroll += $(this).width();
+    });
+
+    if (event) {
+        plans.animate({scrollLeft: newScroll});
+    } else {
+        plans.scrollLeft(newScroll);
+    }
+}
+
 table.ready(function() {
     setTimeout(function() {
-        updateDataHeight();
+        updateDataSize();
         closeAllData();
     }, 50);
 });
 
-
 recursos.on("click", collapseData);
-$(window).resize(updateDataHeight);
+leftArrow.on("click",  {direction: "left"}, scroll);
+rightArrow.on("click", {direction: "right"}, scroll);
 
-/*$('.carrossel').slick({
-    dots: false,
-    infinite: false,
-    arrows: false,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          dots: false
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-});*/
+$(window).resize(updateDataSize);
